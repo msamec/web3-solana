ebe650e337910144e2845e4ee794bb7ffcca69d2
Marko Samec <marko@samec.dev>
2022-05-04 09:48:31 +0200

    feat: send token

---
+++ send_token.js
@@ -0,0 +1,58 @@
+const web3 = require('@solana/web3.js');
+const splToken = require('@solana/spl-token');
+
+(async () => {
+  const connection = new web3.Connection(
+    web3.clusterApiUrl('devnet'),
+    'confirmed',
+  );
+
+   // Generate a new wallet keypair and airdrop SOL
+    const fromWallet = web3.Keypair.generate();
+    const fromAirdropSignature = await connection.requestAirdrop(fromWallet.publicKey, web3.LAMPORTS_PER_SOL);
+
+    // Wait for airdrop confirmation
+    await connection.confirmTransaction(fromAirdropSignature);
+
+    // Generate a new wallet to receive newly minted token
+    const toWallet = web3.Keypair.generate();
+    console.log('to wallet address:', toWallet.publicKey.toString());
+
+    // Create new token mint, we won't have to do this because we are not creating tokens
+    // but needed to do this here for the purpose of example
+    const mint = await splToken.createMint(connection, fromWallet, fromWallet.publicKey, null, 9);
+
+    // Get the token account of the fromWallet address, and if it does not exist, create it
+    const fromTokenAccount = await splToken.getOrCreateAssociatedTokenAccount(
+        connection,
+        fromWallet,
+        mint,
+        fromWallet.publicKey
+    );
+
+    // Get the token account of the toWallet address, and if it does not exist, create it
+    const toTokenAccount = await splToken.getOrCreateAssociatedTokenAccount(connection, fromWallet, mint, toWallet.publicKey);
+
+    // Mint 1 new token to the "fromTokenAccount" account we just created
+    let signature = await splToken.mintTo(
+        connection,
+        fromWallet,
+        mint,
+        fromTokenAccount.address,
+        fromWallet.publicKey,
+        1000000000,
+        []
+    );
+    console.log('mint tx:', signature);
+
+    // Transfer the new token to the "toTokenAccount" we just created
+    signature = await splToken.transfer(
+        connection,
+        fromWallet,
+        fromTokenAccount.address,
+        toTokenAccount.address,
+        fromWallet.publicKey,
+        1000000000,
+        []
+    );
+    console.log('transfer tx:', signature);})();
